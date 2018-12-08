8a51423ad330da2936012cd536d6689a9f1e5508
DDAMDOO <sjl9524@naver.com>
2018-12-08 15:33:29 +0900

    test case gogo

--- unity/suckjjang/Assets/NewBehaviourScript.cs
+++ unity/suckjjang/Assets/NewBehaviourScript.cs
@@ -4,31 +4,24 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //1번 예
+    public Transform target;//추적할 타겟 변수
 
-    public Transform target;
-    //public float distance = 10.0f;
-    //public float height = 5.0f;
+    private Transform tr; //카메라 자신의 변수
 
-    //float heightDamping = 2.0f;
-    //float rotationDamping = 3.0f;
-    // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-        //float currentAngle = transform.eulerAngles.y;
-        //float currentHeight = transform.position.y;
-
-        //float targetAngleY = target.eulerAngles.y;
-        //float targetHeight = target.position.y + height;
-
-        //currentAngleY = Mathf.LerpAngle(currentAngleY, targetAngleY, rotationDamping * Time.deltaTime);
-
-        //Quaternion newRotation = Quaternion.Euler(0, currentAngleY)
+        tr.LookAt(target);
     }
+
+    //public Transform from;
+    //public Transform to;
+
+    //public Vector3 objToTargetVector = to.position - from.position;
+
 }
