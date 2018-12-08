5abbd24e85f237a65c5bb98af77e2abcbe5f694a
DDAMDOO <sjl9524@naver.com>
2018-12-08 14:25:30 +0900

    make preference

--- unity/a/Assets/lookat.cs
+++ unity/a/Assets/lookat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class lookat : MonoBehaviour {
-
+    
     //public Transform target;
 	// Use this for initialization
 	void Start () {
---
+++ unity/suckjjang/Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Transform target;
+    //public float distance = 10.0f;
+    //public float height = 5.0f;
+
+    //float heightDamping = 2.0f;
+    //float rotationDamping = 3.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        //float currentAngle = transform.eulerAngles.y;
+        //float currentHeight = transform.position.y;
+
+        //float targetAngleY = target.eulerAngles.y;
+        //float targetHeight = target.position.y + height;
+
+        //currentAngleY = Mathf.LerpAngle(currentAngleY, targetAngleY, rotationDamping * Time.deltaTime);
+
+        //Quaternion newRotation = Quaternion.Euler(0, currentAngleY)
+    }
+}
